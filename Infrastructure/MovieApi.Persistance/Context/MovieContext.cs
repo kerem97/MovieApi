5ae22a9f7eb2a348e7d8ee486e43e31d23511d9f
Kerem DURAK <kerem.durak@yahoo.com>
2025-05-07 17:18:52 +0300

    Creating

---
+++ Infrastructure/MovieApi.Persistance/Context/MovieContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistance.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=HUAWEI\\SQLEXPRESS;initial catalog=ApiMovieDb;integrated security=true ");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}
