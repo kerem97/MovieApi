27271f049cd3d3ccae46c5610fdbd8e94898330c
Kerem DURAK <kerem.durak@yahoo.com>
2025-05-23 10:41:47 +0300

    up mediatr start

---
+++ Core/MovieApi.Application/Features/Mediator/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using MovieApi.Application.Features.Mediator.Queries.CastQueries;
+using MovieApi.Application.Features.Mediator.Results.CastResults;
+using MovieApi.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.Mediator.Handlers.CastHandlers
+{
+    public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
+    {
+        private readonly MovieContext _context;
+
+        public GetCastByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _context.Casts.FindAsync(request.Id);
+            return new GetCastByIdQueryResult
+            {
+                Id = value.Id,
+                Biography = value.Biography,
+                ImageUrl = value.ImageUrl,
+                Name = value.Name,
+                Overview = value.Overview,
+                Surname = value.Surname,
+                Title = value.Title
+            };
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/Mediator/Handlers/CastHandlers/GetCastQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.Mediator.Queries.CastQueries;
+using MovieApi.Application.Features.Mediator.Results.CastResults;
+using MovieApi.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.Mediator.Handlers.CastHandlers
+{
+    public class GetCastQueryHandler : IRequestHandler<GetCastQuery, List<GetCastQueryResult>>
+    {
+        private readonly MovieContext _context;
+
+        public GetCastQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetCastQueryResult>> Handle(GetCastQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Casts.ToListAsync();
+            return values.Select(x => new GetCastQueryResult
+            {
+                Biography = x.Biography,
+                Id = x.Id,
+                ImageUrl = x.ImageUrl,
+                Name = x.Name,
+                Overview = x.Overview,
+                Surname = x.Surname,
+                Title = x.Title
+            }).ToList();
+        }
+    }
+}
