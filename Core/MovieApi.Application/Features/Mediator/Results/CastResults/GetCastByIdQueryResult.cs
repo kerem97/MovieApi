d913e24fe6d551759a5903ce09a8780ace6ef70b
Kerem DURAK <kerem.durak@yahoo.com>
2025-05-23 10:10:32 +0300

    up

---
+++ Core/MovieApi.Application/Features/Mediator/Results/CastResults/GetCastByIdQueryResult.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.Mediator.Results.CastResults
+{
+    public class GetCastByIdQueryResult : IRequest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public string? Overview { get; set; }
+        public string? Biography { get; set; }
+    }
+}
